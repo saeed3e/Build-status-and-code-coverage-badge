24e6e739c0a60f6d123ffbcd7525f84a4953f199
Mohd Saeed <mohd.saeed@Infoedge.com>
2016-10-06 16:28:35 +0530

    add coverall task in grunt.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,8 +44,7 @@ module.exports = function(grunt) {
 
     //grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
-
-    grunt.registerTask('test', ['jasmine']);
-    grunt.registerTask('default', ['uglify','test']);
+    grunt.loadNpmTasks('grunt-coveralls');
+    grunt.registerTask('test', ['jasmine',"coveralls"]);
 
 };
