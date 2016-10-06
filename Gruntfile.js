e33716e1d40a8b9284b643e221a9d0da47c53283
Mohd Saeed <mohd.saeed@Infoedge.com>
2016-10-06 15:19:14 +0530

    add .travis and coveralls.yml file

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        jasmine: {
+            coverage: {
+                src: 'src/**/*.js',
+                options: {
+                    specs: 'spec/**/*.js',
+                    template: require('grunt-template-jasmine-istanbul'),
+                    templateOptions: {
+                        coverage: 'src/coverage/coverage.json',
+                        //report: 'src/coverage',
+                        report: {
+                            type: 'cobertura',
+                            options: {
+                                dir: 'src/coverage'
+                            }
+                        },
+                        report: {
+                            type: 'cobertura',   // valid types : cobertura, lcov
+                            options: {
+                                dir: 'src/coverage'
+                            }
+                        },
+                        thresholds: {
+                            lines: 5,
+                            statements: 5,
+                            branches: 5,
+                            functions: 5
+                        }
+                    }
+                }
+            },
+        },
+        coveralls: {
+            // Options relevant to all targets
+            options: {
+                // When true, grunt-coveralls will only print a warning rather than
+                // an error, to prevent CI builds from failing unnecessarily (e.g. if
+                // coveralls.io is down). Optional, defaults to false.
+                force: false
+            },
+            your_target: {
+                // LCOV coverage file (can be string, glob or array)
+                src: 'src/coverage/lcov.info'
+            },
+        },
+    });
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    grunt.registerTask('test', ['jasmine']);
+    grunt.registerTask('default', ['test']);
+
+};
